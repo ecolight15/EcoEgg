68e4067e9aeb8f1aed3c074fa75d77ced33dfc42
ecolight15 <github@minecraftuser.jp>
2019-12-21 19:51:11 +0900

    マージミスを修正

--- src/main/java/jp/minecraftuser/ecoegg/listener/HatchingListener.java
+++ src/main/java/jp/minecraftuser/ecoegg/listener/HatchingListener.java
@@ -73,12 +73,6 @@ public class HatchingListener extends ListenerFrame {
         Player player = event.getPlayer();
         Entity ent = event.getRightClicked();
 
-        // LivingEntity出ない場合は何もしない
-        if (!(ent instanceof LivingEntity)) {
-            return;
-        }
-        LivingEntity le = (LivingEntity) ent;
-
         //LivienEntity以外はキャンセル
         if(!(ent instanceof LivingEntity)){
             return;
